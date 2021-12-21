c9cc2a82c280bab5a6c4b26164f048bb57db7047
DrMoE77 <drmo.afifi@gmail.com>
2021-12-21 18:19:04 +1100

    update

--- javascript.js
+++ javascript.js
@@ -1,54 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var specialCharacters = "@%+\\!#$^?)(}{][~-_.";
+  
+var numericCharacters = "0123456789";
+
+var lowerCasedCharacters = "abcdefghijklmnopqrstuvwxyz";
+
+var upperCasedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 function generatePassword() {
   // prompt the user for characters to include in the final password
   // uppercase / lowercase / symbols / numbers
-​  var useUppercase = window.confirm("Do you want to use Uppercase letters?");
+var useUppercase = window.confirm("Do you want to use Uppercase letters?");
   var useLowercase = window.confirm("Do you want to use Lowercase letters?");
   var useSymbols = window.confirm("Do you want to use Symbols letters?");
   var useNumbers = window.confirm("Do you want to use Numbers letters?");
-​
+
   console.log("uppercase?", useUppercase);
   console.log("useLowercase?", useLowercase);
   console.log("useSymbols?", useSymbols);
   console.log("useNumbers?", useNumbers);
-​// ask the user how long the password should be
+
+  // ask the user how long the password should be
+
 var passwordLength = window.prompt("How long will the password be (8-100)?");
-​
+
   console.log("passwordLength", passwordLength);
-​
+
   // generate the password based on what the user chose
   // based on what the user chose we pick the required amount of characters from
   // a character container
-​
-  var uppercaseCharacters = "ABCD";
-  var lowercaseCharacters = "abcd";
-  var symbols = "!@#$";
-  var numbers = "1234";
-​
+
   var selectedCharacters = "";
-​
-  if (useUppercase)
-    selectedCharacters = selectedCharacters + uppercaseCharacters;
-​
-  if (useLowercase)
-    selectedCharacters = selectedCharacters + lowercaseCharacters;
-​
-  if (useSymbols) selectedCharacters = selectedCharacters + symbols;
-​
-  if (useNumbers) selectedCharacters = selectedCharacters + numbers;
-​
-  console.log(selectedCharacters);
-​
-  var finalPassword = ""
+
+  if (useUppercase) selectedCharacters = selectedCharacters + upperCasedCharacters;
+
+  if (useLowercase) selectedCharacters = selectedCharacters + lowerCasedCharacters;
+
+  if (useSymbols) selectedCharacters = selectedCharacters + specialCharacters;
+
+  if (useNumbers) selectedCharacters = selectedCharacters + numericCharacters;
+
+
+
+  var finalPassword = createPassword(passwordLength, selectedCharacters);
+
+
+  console.log(finalPassword);
+  
+
+ 
   
   // write a for loop that cycles from 0 to the expected length of the password and 
   // at every iteration of the loop add a random character from selectedCharacters
-  // to the finalPassword
-​
+  // to the finalPassword​
   return finalPassword;
+}
+
+
+function createPassword(passwordLength, selectedCharacters){
+  console.log(passwordLength, selectedCharacters);
+  var pass = "";
+  for (var i= 0; i < passwordLength; i++) {
+   var rand = Math.floor(Math.random()*selectedCharacters.length);
+   console.log(rand);
+   pass += selectedCharacters[rand];
+  }
+  console.log(pass);
+
+  return pass;
+
 }
 //Collapse
 // Write password to the #password input
